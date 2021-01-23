f51e1d23e166df181923561c8f795c87f4bf02e5
Joohoon Kim <jayjhkim7@gmail.com>
2021-01-22 20:21:21 -0500

    fix landing page grid and cards

--- client/src/components/propertyCard.component.jsx
+++ client/src/components/propertyCard.component.jsx
@@ -14,24 +14,20 @@ const PropertyCard = ({ address, images, id }) => {
 
   return (
     <div className="property-card">
-      <ul className="cards">
-        <li className="cards_item" style={{ minWidth: '20rem' }}>
-          <div className="card">
-            <div className="card_image">
-              <img src={images[0]} alt="property images" />
-              <div className="card_content">
-                <h2 className="address">{address.street}</h2>
-                <p className="card_text">
-                  Click the button to see auction status and place your bid
-                </p>
-                <button className="btn" onClick={handleClick}>
-                  Details
-                </button>
-              </div>
-            </div>
-          </div>
-        </li>
-      </ul>
+      <div className="card">
+        <div className="card_image">
+          <img src={images[0]} alt="property images" />
+        </div>
+        <div className="card_content">
+          <h2 className="address">{address.street}</h2>
+          <p className="card_text">
+            Click the button to see auction status and place your bid
+          </p>
+          <button className="btn" onClick={handleClick}>
+            Details
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
